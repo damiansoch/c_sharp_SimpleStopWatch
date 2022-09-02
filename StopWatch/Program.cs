e901e6bd16706a055ff788f1fa96b018a98aca33
Damian Socha <damiansoch@hotmail.com>
2022-09-02 08:30:28 +0100

    Add project files.

---
+++ StopWatch/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StopWatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Put the time in seconds!");
+            int time = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            Watch watch = new Watch(time);
+            watch.Start();
+            Console.ReadKey();
+            watch.Stop();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StopWatch/Watch.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace StopWatch
+{
+    public class Watch
+    {
+        public Watch(int length)
+        {
+            Length = length;
+        }
+
+        public void Start()
+        {
+            if (!isCounting)
+            {
+                Console.WriteLine("Is counting!\n");
+                curDuration = Length;
+                timer = new Timer(TimerCallback, null, 0, 1000);
+
+                //isCounting = true;
+            }
+        }
+        public void Stop()
+        {
+            Console.WriteLine("Stopped @ {0}", curDuration);
+            curDuration = Length;
+            timer.Dispose();
+            isCounting = false;
+        }
+
+
+        private void TimerCallback(Object o)
+        {
+            if (curDuration > 0)
+            {
+                Console.Clear();
+                curDuration--;
+                Console.WriteLine("\n\n\n\n\n\n\n\n");
+                Console.Write($"                         {curDuration}");
+                GC.Collect();
+            }
+            else
+            {
+                Console.Clear();
+
+                Stop();
+            }
+        }
+
+        protected int Length { get; set; }
+        Timer timer;
+        protected bool isCounting = false;
+        int curDuration;
+    }
+}
